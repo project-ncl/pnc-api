5b4a6fbcae7eae9fc0e98e103b57367588a20a4a
Jozef Mrazek <jmrazek@redhat.com>
2026-02-02 11:23:52 +0100

    Revert "[NCL-9428] further validation"
    This reverts commit 1a45f1baee2cf7ba0cb03da67c851f2120925308.

--- api/src/main/java/org/jboss/pnc/api/orch/dto/BuildResultRest.java
+++ api/src/main/java/org/jboss/pnc/api/orch/dto/BuildResultRest.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import org.jboss.pnc.api.enums.orch.CompletionStatus;
@@ -58,17 +59,18 @@ public class BuildResultRest implements Serializable {
 
     private BuildExecutionConfigurationRest buildExecutionConfiguration;
 
-    private RepourResultRest repourResult;
-
-    private EnvironmentDriverResultRest environmentDriverResult;
-
     private BuildDriverResultRest buildDriverResult;
 
     private @Valid RepositoryManagerResultRest repositoryManagerResult;
 
-    private @Valid List<Artifact> attachments;
+    private EnvironmentDriverResultRest environmentDriverResult;
 
-    private Map<String, String> extraAttributes;
+    private RepourResultRest repourResult;
+
+    private List<@NotNull(groups = { WhenCreatingNew.class, WhenImporting.class }) Artifact> attachments;
+
+    private Map<@NotBlank(groups = { WhenCreatingNew.class, WhenImporting.class }) String, @NotNull(
+            groups = { WhenCreatingNew.class, WhenImporting.class }) String> extraAttributes;
 
     @Override
     public String toString() {
